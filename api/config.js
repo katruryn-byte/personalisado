89b6b896dd756a2e2dfe5e18a6b67c9082752a21
katruryn-byte <katruryn@gmail.com>
2026-05-26 15:18:22 -0300

    Update config.js

--- api/config.js
+++ api/config.js
@@ -2,8 +2,5 @@ module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   if (req.method === 'OPTIONS') return res.status(200).end();
-
-  return res.status(200).json({
-    publicKey: process.env.MP_PUBLIC_KEY
-  });
+  return res.status(200).json({ publicKey: process.env.MP_PUBLIC_KEY });
 }
